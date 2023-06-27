484337b3b3c23724e53ff5c9e24d4d3078aefc9c
Yakup <yakupjoraev@gmail.com>
2023-06-27 11:32:07 +0300

    added index page

--- js/main.js
+++ js/main.js
@@ -5,44 +5,53 @@ const swiper = new Swiper(".swiper", {
   },
 });
 
+function tabsContainer() {
+  const container = document.querySelector('.tabs')
+  if (!container) {
+    return null
+  }
 
-function tabs(headerSelector, tabSelector, contentSelector, activeClass, display = 'flex') {
-  const header = document.querySelector(headerSelector),
-    tab = document.querySelectorAll(tabSelector),
-    content = document.querySelectorAll(contentSelector)
-  function hideTabContent() {
-    content.forEach(item => {
-      item.style.display = 'none'
-    });
-    tab.forEach(item => {
-      item.classList.remove(activeClass)
-    });
-  }
-  function showTabContent(i = 0) {
-    content[i].style.display = display
-    tab[i].classList.add(activeClass)
-  }
-  hideTabContent()
-  showTabContent()
-  header.addEventListener('click', e => {
-    const target = e.target
-    if (target.classList.contains(tabSelector.replace(/\./, '')) ||
-      target.parentNode.classList.contains(tabSelector.replace(/\./, ''))) {
-      tab.forEach((item, i) => {
-        if (target == item || target.parentNode == item) {
-          hideTabContent()
-          showTabContent(i)
-        }
+  function tabs(headerSelector, tabSelector, contentSelector, activeClass, display = 'flex') {
+    const header = document.querySelector(headerSelector),
+      tab = document.querySelectorAll(tabSelector),
+      content = document.querySelectorAll(contentSelector)
+    function hideTabContent() {
+      content.forEach(item => {
+        item.style.display = 'none'
+      });
+      tab.forEach(item => {
+        item.classList.remove(activeClass)
       });
     }
-  })
+    function showTabContent(i = 0) {
+      content[i].style.display = display
+      tab[i].classList.add(activeClass)
+    }
+    hideTabContent()
+    showTabContent()
+    header.addEventListener('click', e => {
+      const target = e.target
+      if (target.classList.contains(tabSelector.replace(/\./, '')) ||
+        target.parentNode.classList.contains(tabSelector.replace(/\./, ''))) {
+        tab.forEach((item, i) => {
+          if (target == item || target.parentNode == item) {
+            hideTabContent()
+            showTabContent(i)
+          }
+        });
+      }
+    })
+  }
+
+  // ПЕРВЫЙ аргумент - класс всего нашего хедера табов.
+  // ВТОРОЙ аргумент - класс конкретного элемента, при клике на который будет переключатся таб.
+  // ТРЕТИЙ аргумент - класс того блока, который будет переключаться.
+  // ЧЕТВЕРТЫЙ аргумент - класс активности, который будет добавлятся для таба, который сейчас активен.
+  tabs('.tabs__header', '.tabs__header-item', '.tabs__content-item', 'active')
+
 }
 
-// ПЕРВЫЙ аргумент - класс всего нашего хедера табов.
-// ВТОРОЙ аргумент - класс конкретного элемента, при клике на который будет переключатся таб.
-// ТРЕТИЙ аргумент - класс того блока, который будет переключаться.
-// ЧЕТВЕРТЫЙ аргумент - класс активности, который будет добавлятся для таба, который сейчас активен.
-tabs('.tabs__header', '.tabs__header-item', '.tabs__content-item', 'active')
+tabsContainer()
 
 function productPreviewSlider() {
   const container = document.querySelector('.cardproduct-section-block-slider');
@@ -78,24 +87,30 @@ function productPreviewSlider() {
 productPreviewSlider()
 
 function init() {
+  let center = [56.868918274719825, 38.244967662040686];
 
   let map = new ymaps.Map('map', {
-    center: [55.75554289958026, 37.64131907421875],
+    center: center,
     zoom: 9
-
   });
+
+  let placemark = new ymaps.Placemark(center, {}, {
+    iconLayout: 'default#image',
+    iconImageHref: '../img/location-icon.svg',
+    iconImageSize: [42, 42],
+    iconImageOffset: [-21, -41]
+  })
+
+  map.geoObjects.add(placemark);
+
+  map.controls.remove('geolocationControl'); // удаляем геолокацию
+  map.controls.remove('searchControl'); // удаляем поиск
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+  map.controls.remove('typeSelector'); // удаляем тип
+  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+  map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
 }
 
 ymaps.ready(init);
-
-function init() {
-  let map = new ymaps.Map1('map1', {
-    center: [55.75554289958026, 37.64131907421875],
-    zoom: 9
-
-  });
-}
-
-ymaps.ready(init);
-
-
